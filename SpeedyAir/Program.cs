3c6f4af09b36d128002355ed35097c10b5633dff
arsalankhaliq <khaliqarsalan@gmail.com>
2023-04-19 16:54:21 -0400

    Created the flight class | Loaded the flight and outputted it to the console

--- SpeedyAir/Program.cs
+++ SpeedyAir/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SpeedyAir
 {
@@ -6,7 +7,20 @@ namespace SpeedyAir
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var flights = new List<Flight>
+            {
+                new Flight(1, "YUL", "YYZ", 1),
+                new Flight(2, "YUL", "YYC", 1),
+                new Flight(3, "YUL", "YVR", 1),
+                new Flight(4, "YUL", "YYZ", 2),
+                new Flight(5, "YUL", "YYC", 2),
+                new Flight(6, "YUL", "YVR", 2)
+            };
+
+            foreach (var flight in flights)
+            {
+                Console.WriteLine($"Flight: {flight.FlightNumber}, departure: {flight.DepartureCity}, arrival: {flight.ArrivalCity}, day: {flight.Day} ");
+            }
         }
     }
 }
